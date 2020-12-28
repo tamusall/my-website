074f39c569bfef7430feb3d86e3748dc6148639d
Travis Musall <tamusall@gmail.com>
2020-12-28 17:01:12 -0500

    added typescript + tailwind ✨

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-lg font-extrabold">
+        Hello World
+      </h1>
+    </div>
+  )
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.[j,t]s', './components/**/*.[j,t]s'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
